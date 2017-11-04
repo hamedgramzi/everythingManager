c40c142e22d14415cd2e778fd8e2c16371ba663d
hamed <hamedgramzi@gmail.com>
2017-11-04 19:09:14 +0330

    change method dp convertors to static

--- everythingmanager/src/main/java/ir/atitec/everythingmanager/utility/Util.java
+++ everythingmanager/src/main/java/ir/atitec/everythingmanager/utility/Util.java
@@ -113,12 +113,12 @@ public class Util {
         }
     }
 
-    public int dpToPx(Context context, int dp) {
+    public static  int dpToPx(Context context, int dp) {
         DisplayMetrics displayMetrics = context.getResources().getDisplayMetrics();
         return Math.round(dp * (displayMetrics.xdpi / DisplayMetrics.DENSITY_DEFAULT));
     }
 
-    public int pxToDp(Context context, int px) {
+    public static int pxToDp(Context context, int px) {
         DisplayMetrics displayMetrics = context.getResources().getDisplayMetrics();
         return Math.round(px / (displayMetrics.xdpi / DisplayMetrics.DENSITY_DEFAULT));
     }
